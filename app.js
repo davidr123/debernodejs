ceff781672bc0ceb211edcc7ddb79ab9573cab2e
davidr123 <davidradaalejandro@gmail.com>
2019-01-13 14:52:06 -0500

    primer debrernodej

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require ('express');
+var exphbs = require ('express-handlebars');
+var app = express();
+
+const knex= require('./db/knex');
+
+app.set('port', process.env.PORT || 3000);
+
+app.engine('handlebars', exphbs({defaultLayout:
+                                 'main'}));
+app.set('view engine','handlebars');
+
+
+//ENRUTAMIENTO
+
+app.get('/',function(req, res){
+   /* res.type('text/plain');
+    res.send('Mi página principal');*/
+    res.render('home');
+});
+
+app.get('/user',function(req, res){
+   /* res.type('text/plain');
+    res.send('Mi página principal');*/
+    knex('usuario')
+    .select()
+    .then(usuario =>{
+        res.render('usuario', {objUsers: usuario});
+    });
+    
+   // res.render('usuario');
+});
+
+
+
+
+
+
+app.get('/about',function(req,res){
+    /*res.type('text/plain');
+    res.send('About my page');*/
+    res.render('about');
+});
+
+
+        app.listen(app.get('port'), function(){
+            console.log('Express on localhost:' + 
+                        app.get('port'));
+        });
+
+
+//ARCHIVOS ESTÁTICOS
+app.use(express.static(__dirname + '/public'));
